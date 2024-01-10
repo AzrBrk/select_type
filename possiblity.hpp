a3d6be0073a03907e76f0af732d28b05bed2bf94
AzrBrk <pbrink51@gmail.com>
2024-01-11 07:59:52 +0800

    Add files via upload

--- possiblity.hpp
+++ possiblity.hpp
@@ -1,64 +1,103 @@
-#pragma once
+#include<type_traits>
 
-#include<memory>
-#include"select_type.hpp"
-#include<vector>
-#include"function_impl.hpp"
-
-namespace possible
+#include<tuple>
+#include<iostream>
+namespace possibilities
 {
-	template<class node> struct possible_node 
-	{
-		const size_t idx;
-		node first_node;
+    //需要C++20以上
+    //原理是用std::is_constructible猜测类的成员类型
+    namespace possibilities_utilities
+    {
+        struct inits
+        {
+            template<class T> operator T();
+        };
+        //这个类型用来试探某种类型是不是类的第N个成员类型
+        //由于std::is_constructible隐式转换也会true，所以这里禁止隐式转换以求结果精确
+        template<class T> struct convert_forbid
+        {
+            using type = T;
+            template<class U,class En = std::enable_if_t<std::is_same_v<T, U>>> operator U();
+        };
+        //计算成员数量,生成构造列表
+        template<bool, class T, class construct_list, size_t cnt> struct count_size{};
+        template<class T, template<class...> class construct_list, size_t cnt, class ...types>
+        struct count_size<true, T, construct_list<types...>, cnt>
+        {
+            static constexpr size_t value = cnt;
+            using CTL = construct_list<T, types...>;
+        };
+        template<class T, template<class ...> class construct_list, size_t cnt, class First, class ...rest>
+        struct count_size<false, T, construct_list<First, rest...>, cnt>
+        {
+            using next = count_size<std::is_constructible_v<T, First, rest...>,T, std::conditional_t<
+                std::is_constructible_v<T, First, rest...>, construct_list<First, rest...>, construct_list<rest...>>, 
+                (std::is_constructible_v<T, First, rest...> ? cnt : cnt - 1)>;
+            static constexpr size_t value =next::value;
+            using CTL = typename next::CTL;
+        };
+        template<size_t N, class pre, class bck, class T> struct replace_at{};
 
-		exp_iterator<node> iter() {
-			exp_iterator it{ first_node };
-			it[idx];
-			return it;
-		}
-	};
-	
-	template<class Node, typename T>
-		requires (exp_is_one_of<T, typename Node::element_type_list>::value)
-	Node from_value_to_node(T const& v) 
-	{
-		auto node_maker = [](auto ...x) {
-			return make_element_node(shared_constructor(), x...);
-			};
+        template<size_t N, template<class...> class pre, template<class...> class bck, class RP, class bck_first, class ...preargs, class... bckargs>
+        struct replace_at<N, pre<preargs...>, bck<bck_first, bckargs...>, RP>
+        {
+            using type = typename replace_at<N - 1, pre<preargs..., bck_first>, bck<bckargs...>, RP>::type;
+        };
 
-		auto efb = exp_bind::bind(function_impl::realize_meta<typename Node::element_type_list>(node_maker));
+        template<template<class...> class pre, template<class...> class bck, class RP, class bck_first, class ...preargs, class... bckargs>
+        struct replace_at<0, pre<preargs...>, bck<bck_first, bckargs...>, RP>
+        {
+            using type = pre<preargs..., RP, bckargs...>;
+        };
 
-		size_t idx = exp_is_one_of<T, typename Node::element_type_list>::index;
+        template<class TL, template<class...> class Name> struct as_ {};
+        template<template<class...>class TL, template<class...>class Name, class ...Args> struct as_<TL<Args...>, Name>
+        { using type = Name<Args...>; };
+        //在列表种选择元素
+        template<size_t N, class TL> struct select_element { using type = std::tuple_element_t<N, typename as_<TL, std::tuple>::type>; };
+    }
+    using namespace possibilities_utilities;
 
-		efb[idx] = v;
-		
-		return efb.apply_func();
-	}
+    template<class ...> struct tl {};
 
-	//Possiblilities is a class that can stored a series types of data infinitly, the data stored inside can be accessed 
-	// by non compiled time index
-	//Note: no repeat type in a typelist
-	//Note: do not regard this as a repleacement for std::variant, the creation of exp_nodes costs a lot of memory
-	template<class ...Typs> 
-	class possibilities: public std::vector<possible_node<exp_shared_node<Typs...>>>
-	{
-	public:
-		static_assert(is_unique_type_list<exp_list<Typs...>>::value, "Posibilities requires a typelist with no type repeat");
+    template<class T> using count_size_type = count_size<false, std::decay_t<T>, tl<inits, inits, inits, inits, inits, inits, inits, inits, inits, inits>, 10>;
+    template<class T> constexpr size_t size() {
+        return count_size_type<T>::value;
+    }
+    //工具类，测试T的N个成员是否为Mem_type
+    template<size_t N, class T, class Mem_type> struct is_constructible_at
+    {
+        using construct_list = typename count_size_type<T>::CTL;
+        using chk_constructible = typename replace_at<N + 1, tl<>, construct_list, convert_forbid<Mem_type>>::type;
+        using constructible_type = typename as_<chk_constructible, std::is_constructible>::type;
+        static constexpr bool value = constructible_type::value;
 
-		template<class T> void push(T const& v) 
-		{
-			static_assert(exp_is_one_of<T, exp_list<Typs...>>::value, "Provided value is not one of a type from the typelist constructed");
-			using possible_type = possible_node<exp_shared_node<Typs...>>;
+    };
+    //工具类，用一个列表mem_type_list逐个测试is_constructilbe_at，得到T的第N个成员的类型
+    template<bool constructible_, size_t N, class T, class mem_type_list> struct constructible_at_try {};
+    template<bool constructible_, size_t N, class T, template<class...> class mem_type_list, class first, class ...Args>
+    struct constructible_at_try<constructible_, N, T, mem_type_list<first, Args...>>
+    {
+        static constexpr bool cstible = is_constructible_at<N, T, first>::value;
+        using type = typename constructible_at_try<cstible, N, T,
+            std::conditional_t<cstible, mem_type_list<first, Args...>, mem_type_list<Args...>>>::type;
+    };
 
-			this->push_back(possible_type{ 
-				exp_is_one_of<T, exp_list<Typs...>>::index,
-				from_value_to_node<exp_shared_node<Typs...>>(v) 
-				}
-			);
-		}
-		template<typename ...Args> void push(Args... args) {
-			(push(std::move(args)), ...);
-		}
-	};
+    template<size_t N, class T, template<class...> class mem_type_list, class first, class ...Args> 
+    struct constructible_at_try<true, N, T, mem_type_list<first, Args...>> {using type = first;};
+    //工具类，生成成员列表
+    template<size_t N, class T, class TL, class saved> struct make_construct_list {};
+    template<class T, template<class...> class TL, template<class...> class saved, class ...Args, class ...savedArgs> 
+    struct make_construct_list<0, T, TL<Args...>, saved<savedArgs...>>
+    {
+        using current_type = typename constructible_at_try<false, 0, T, TL<Args...>>::type;
+        using type = saved<current_type, savedArgs...>;
+    };
+    template<size_t N, class T, template<class...> class saved, class TL, class ...Args> struct make_construct_list<N, T, TL, saved<Args...>>
+    {
+        using current_type = typename constructible_at_try<false, N, T, TL>::type;
+        using type = typename make_construct_list<N - 1, T, TL, saved<current_type, Args...>>::type;
+    };
+
+    template<class T, class Poss> using possibility = typename make_construct_list<possibilities::size<T>() - 1, T, Poss, tl<>>::type;
 }
